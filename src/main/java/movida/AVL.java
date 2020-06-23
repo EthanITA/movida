5f6e6a9c3fc8826a35ff37dba0e8d84c85aacb78
Marco Dong <marco@n552vw.station>
2020-06-23 16:51:15 +0200

    creazione classe AVL e HashIndirizzamentoAperto

---
+++ src/main/java/movida/AVL.java
@@ -0,0 +1,4 @@
+package movida;
+
+public class AVL {
+}
---
+++ src/main/java/movida/HashIndirizzamentoAperto.java
@@ -0,0 +1,15 @@
+package movida;
+
+/**
+ * HashIndirizzamentoAperto
+ */
+public class HashIndirizzamentoAperto {
+    public void insert() {
+    }
+
+    public void delete() {
+    }
+
+    public void lookup() {
+    }
+}
